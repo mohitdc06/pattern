6b2266643b20c0e216f15fc7c9d4dabd928046c8
“mohitdc06” <mohitdc0602@gmail.com>
2024-08-05 23:37:59 +0530

    hollow triangle

--- patterns.java
+++ patterns.java
@@ -2,19 +2,23 @@ public class patterns {
     public static void Kpattern(int n) {
         //1st half
         for (int i = 1; i <= (n + 1) / 2; i++) {
-            for (int str =1 ; str <= n/2+2-i; str++) {
+            for (int str = 1; str <= n / 2 + 2 - i; str++) {
                 System.out.print(" * ");
             }
             System.out.println();
         }
-        
-         //2nd half
-        for (int i = 1; i <= n/2; i++) {
-            for (int str = 1; str <=i+1; str++) {
+
+        //2nd half
+        for (int i = 1; i <= n / 2; i++) {
+            for (int str = 1; str <= i + 1; str++) {
                 System.out.print(" * ");
             }
             System.out.println();
         }
+
+    }
+    
+    public static void hollowtriangle(int n) {
         
     }
     
